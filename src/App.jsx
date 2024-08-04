b2a3b7b64c532f557319ed48942a1ca5864c9dda
Uzairkazi695 <167708998+Uzairkazi695@users.noreply.github.com>
2024-08-05 00:17:10 +0530

    Redux-Tool-Kit setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
 import "./App.css";
+import { useEffect, useState } from "react";
+import authService from "./appwrite/auth";
+import { Footer, Header } from "./components";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  
-  
-  
-  
-  return (
-    <>
-      <h1>Hello world!</h1>
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-
-    </>
-  );
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="w-full block">
+      <Header />
+      <main>
+        {/* <Outlet /> */}
+      </main>
+      <Footer />
+    </div></div>
+  ) : null;
 }
 
 export default App;
